d9f719d0ac01d18e85417b67d6fbcd00dbb2b06a
shipzhou <cong_zc@yunzhijia.com>
2020-01-17 17:12:59 +0800

    init project

---
+++ src/main/java/com/shipzhou/zblog/ZblogApplication.java
@@ -0,0 +1,13 @@
+package com.shipzhou.zblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZblogApplication.class, args);
+    }
+
+}
